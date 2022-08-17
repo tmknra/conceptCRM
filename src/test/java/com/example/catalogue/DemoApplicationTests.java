3534576f7c390b6c69eba433d1f1a036cb163d00
architect3232 <mr.moron56@gmail.com>
2022-08-17 23:19:04 +0500

    update titles

--- src/test/java/com/example/catalogue/DemoApplicationTests.java
+++ src/test/java/com/example/catalogue/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.catalogue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CatalogueApplicationTests {
 
 	@Test
 	void contextLoads() {
